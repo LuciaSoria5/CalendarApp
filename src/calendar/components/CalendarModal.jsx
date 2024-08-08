7e0e0ed9c6d65a6c121bf12c74d7bc86bd944459
Lucia Soria <luciacsoria5@gmail.com>
2024-08-08 16:22:40 -0300

    Add <DatePicker/>

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -1,5 +1,10 @@
+import { addHours } from 'date-fns';
 import { useState } from 'react';
-import Modal from 'react-modal'
+
+import Modal from 'react-modal';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+
 
 const customStyles = {
     content: {
@@ -18,6 +23,27 @@ export const CalendarModal = () => {
 
     const [isOpen, setIsOpen] = useState(true);
 
+    const [formValues, setFormValues] = useState({
+        title: 'Luci',
+        notes:'soria',
+        start: new Date(),
+        end: addHours(new Date(), 2)
+    });
+
+    const onInputChange = ({ target }) => {
+        setFormValues({
+            ...formValues,
+            [target.name]: target.value
+        })
+    }
+
+    const onDateChange = ( event, changing ) => {
+        setFormValues({
+            ...formValues,
+            [changing]: event
+        })
+    }
+
     const onCloseModal = () => {
         console.log('Modal cerrado')
         setIsOpen( false );
@@ -32,9 +58,71 @@ export const CalendarModal = () => {
         overlayClassName="modal-fondo"
         closeTimeoutMS={ 200 }
     >
-        <h1>Hola!!!</h1>
+        <h1> Nuevo evento </h1>
+        
         <hr />
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi dolor cupiditate officia nemo harum odio aperiam eum, corrupti, maiores maxime perspiciatis. Voluptate, culpa incidunt? Vero impedit eum earum nesciunt fugit.</p>
+        <form className="container">
+
+            <div className="form-group mb-2">
+                <label>Fecha y hora inicio</label>
+                <DatePicker 
+                    selected={ formValues.start }
+                    onChange={ (event) => onDateChange( event, 'start' ) }
+                    className="form-control"
+                    wrapperClassName="w-100"
+                    dateFormat="Pp"
+                />
+            </div>
+
+            <div className="form-group mb-2">
+                <label>Fecha y hora fin</label>
+                <DatePicker 
+                    minDate={ formValues.start }
+                    selected={ formValues.end }
+                    onChange={ (event) => onDateChange( event, 'end' ) }
+                    className="form-control"
+                    wrapperClassName="w-100"
+                    dateFormat="Pp"
+                />
+            </div>
+
+            <hr />
+            <div className="form-group mb-2">
+                <label>Titulo y notas</label>
+                <input 
+                    type="text" 
+                    className="form-control"
+                    placeholder="Título del evento"
+                    name="title"
+                    autoComplete="off"
+                    value={ formValues.title }
+                    onChange={ onInputChange }
+                />
+                <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+            </div>
+
+            <div className="form-group mb-2">
+                <textarea 
+                    type="text" 
+                    className="form-control"
+                    placeholder="Notas"
+                    rows="5"
+                    name="notes"
+                    value={ formValues.notes }
+                    onChange={ onInputChange }
+                ></textarea>
+                <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+            </div>
+
+            <button
+                type="submit"
+                className="btn btn-outline-primary btn-block"
+            >
+                <i className="far fa-save"></i>
+                <span> Guardar</span>
+            </button>
+
+        </form>
 
     </Modal>
   )
