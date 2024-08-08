e29a5b9f12148844a1bcf6d60b98f17fa027960a
Lucia Soria <luciacsoria5@gmail.com>
2024-08-08 15:20:23 -0300

    Add calendar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,9 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import { LoginPage } from "../auth";
-import { CalendarPage } from "../calendar";
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { LoginPage } from '../auth';
+import { CalendarPage } from '../calendar';
 
 export const AppRouter = () => {
-    const authStatus = 'not-authenticated';
+    const authStatus = 'authenticated';
     
   return (
     <Routes>
