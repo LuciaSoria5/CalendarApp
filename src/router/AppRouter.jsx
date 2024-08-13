c51c3bcb30047c9cdfc7b5f12fd8124d90613a8d
Lucia Soria <luciacsoria5@gmail.com>
2024-08-13 15:48:01 -0300

    Add useForm

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,7 +3,7 @@ import { LoginPage } from '../auth';
 import { CalendarPage } from '../calendar';
 
 export const AppRouter = () => {
-    const authStatus = 'authenticated';
+    const authStatus = 'not-authenticated'; // autenticated, not-authenticated, checking
     
   return (
     <Routes>
